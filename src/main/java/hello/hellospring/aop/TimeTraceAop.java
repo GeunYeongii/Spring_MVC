6aad04d7ec014c483530eeb910667193e7d22015
taranndus <taranndus@gmail.com>
2021-07-15 02:31:05 +0900

    Apply AOP to TimeTraceAop.

--- src/main/java/hello/hellospring/aop/TimeTraceAop.java
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -1,2 +1,25 @@
-package hello.hellospring.aop;public class TimeTraceAop {
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* hello.hellospring..*(..))")  // hello.hellospring 패키지 하위에 있는 것들은 모두 타겟팅 대상
+    public Object excut(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START : " + joinPoint.toString());
+        try {
+            //  proceed to next method
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("END : " + joinPoint.toString() + " " + timeMs + "ms");
+        }
+    }
 }
